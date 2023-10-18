8b36ef4009623115245e5334f93365d1916b3357
Rabemanantsoa Hobiniaina <rabhob@yahoo.fr>
2023-10-18 16:38:27 +0200

    writer implementation

--- Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/WriteSymptomDataToFile.java
@@ -2,24 +2,21 @@ import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Map;
 public class WriteSymptomDataToFile implements ISymptomWriter{
-	private String filepath;
-	public WriteSymptomDataToFile(String filepath) {
-		this.filepath=filepath;
-	}
+		
 	@Override	
 	public void writesymptoms(Map<String,Integer>symptoms){
-		if (filepath!= null) {		
+				
 		try {	
-		FileWriter	writer = new FileWriter(filepath);
+		FileWriter	writer = new FileWriter("result.out");
 			for (Map.Entry<String, Integer> symptom : symptoms.entrySet()) {
 				
-				writer.write(symptom.getKey() + " " + symptom.getValue() + "\n");
+				writer.write(symptom.getKey() + " : " + symptom.getValue() + "\n");
 			}
 				writer.close();
 		} catch (IOException e) {
 				e.printStackTrace();
-		}
+	    }
 	}
 }
-}
+
 
